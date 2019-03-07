218cba2235adb0302a8b4189a23d3bcfb1a9175d
Yashwardhan Gupta <eternalblizzard1998@gmail.com>
2019-03-08 02:45:45 +0530

    Login Completed

--- peertopeer/src/peertopeer/Client.java
+++ peertopeer/src/peertopeer/Client.java
@@ -22,107 +22,95 @@ public class Client {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-          try
-        { 
-            Scanner scn = new Scanner(System.in); 
-              
-            // getting localhost ip 
-//            InetAddress ip = InetAddress.getByName("localhost"); 
-      
-            // establish the connection with server port 5056 
-            Socket s = new Socket("172.19.16.237", 5057); 
-            
-            // obtaining input and out streams 
-            DataInputStream dis = new DataInputStream(s.getInputStream()); 
-            DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
-      
-            // the following loop performs the exchange of 
-            // information between client and client handler 
-            while (true)  
-            { 
-                String received = dis.readUTF(); 
+        try {
+            Scanner scn = new Scanner(System.in);
+
+            // getting localhost ip
+//            InetAddress ip = InetAddress.getByName("localhost");
+
+            // establish the connection with server port 5056
+            Socket s = new Socket("172.19.17.160", 5057);
+
+            // obtaining input and out streams
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+            // the following loop performs the exchange of
+            // information between client and client handler
+            while (true) {
+                String received = dis.readUTF();
                 received.trim();
-                if(received.equals("1"))
-                {
+                if (received.equals("1")) {
                     String macadd = getmac();
                     dos.writeUTF(macadd);
-                }
-                else
-                {
+                } else {
                     System.out.println(received);
-                    String tosend = scn.nextLine(); 
+                    String tosend = scn.nextLine();
                     dos.writeUTF(tosend);
-                    if(tosend.equals("Exit")) 
-                    { 
-                        System.out.println("Closing this connection : " + s); 
-                        s.close(); 
-                        System.out.println("Connection closed"); 
-                        break; 
-                    } 
+                    if (tosend.equals("Exit")) {
+                        System.out.println("Closing this connection : " + s);
+                        s.close();
+                        System.out.println("Connection closed");
+                        break;
+                    }
 
-                    // printing date or time as requested by client 
-                    String received2 = dis.readUTF(); 
+                    // printing date or time as requested by client
+                    String received2 = dis.readUTF();
                     System.out.println(received2);
                 }
-                /*String tosend = scn.nextLine(); 
-                dos.writeUTF(tosend); 
-                  
-                // If client sends exit,close this connection  
-                // and then break from the while loop 
-                if(tosend.equals("Exit")) 
-                { 
-                    System.out.println("Closing this connection : " + s); 
-                    s.close(); 
-                    System.out.println("Connection closed"); 
-                    break; 
-                } 
-                  
-                // printing date or time as requested by client 
-                String received = dis.readUTF(); 
-                System.out.println(received); */
-            } 
-              
-            // closing resources 
-            scn.close(); 
-            dis.close(); 
-            dos.close(); 
-        }catch(Exception e){ 
-            e.printStackTrace(); 
-        } 
+                /*
+                 * String tosend = scn.nextLine(); dos.writeUTF(tosend);
+                 * 
+                 * // If client sends exit,close this connection // and then break from the
+                 * while loop if(tosend.equals("Exit")) {
+                 * System.out.println("Closing this connection : " + s); s.close();
+                 * System.out.println("Connection closed"); break; }
+                 * 
+                 * // printing date or time as requested by client String received =
+                 * dis.readUTF(); System.out.println(received);
+                 */
+            }
+
+            // closing resources
+            scn.close();
+            dis.close();
+            dos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
-    public static String getmac()
-    {
+
+    public static String getmac() {
         InetAddress ip;
-        String macadd="";
+        String macadd = "";
         try {
-                
+
             ip = InetAddress.getLocalHost();
-            //System.out.println("Current IP address : " + ip.getHostAddress());
-            
+            // System.out.println("Current IP address : " + ip.getHostAddress());
+
             NetworkInterface network = NetworkInterface.getByInetAddress(ip);
-                
+
             byte[] mac = network.getHardwareAddress();
-                
-            //System.out.print("Current MAC address : ");
-                
+
+            // System.out.print("Current MAC address : ");
+
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < mac.length; i++) {
-                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));		
+                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
             }
-            //System.out.println(sb.toString());
+            // System.out.println(sb.toString());
             macadd = sb.toString();
-                
+
         } catch (UnknownHostException e) {
-            
+
             e.printStackTrace();
-            
-        } catch (SocketException e){
-                
+
+        } catch (SocketException e) {
+
             e.printStackTrace();
-                
+
         }
         return macadd;
     }
-    
+
 }
--- peertopeer/src/peertopeer/Server.java
+++ peertopeer/src/peertopeer/Server.java
@@ -5,147 +5,195 @@
  */
 package peertopeer;
   
+import java.awt.event.MouseListener;
 import java.io.*; 
 import java.text.*; 
 import java.util.*; 
 import java.net.*; 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
   
 /**
  *
  * @author Yash
  */
-public class Server
-{
-    public static void main(String[] args) throws IOException  
-    { 
-        // server is listening on port 5056 
-        ServerSocket ss = new ServerSocket(5057); 
-          
-        // running infinite loop for getting 
-        // client request 
-        while (true)  
-        { 
-            Socket s = null; 
-              
-            try 
-            { 
-                // socket object to receive incoming client requests 
-                s = ss.accept(); 
-                System.out.println("A new client is connected : " + s); 
-//                String socket_info = s.toString();
-                System.out.println(s.getInetAddress());
-                String temp=s.getInetAddress().toString();
-                String ip=temp.substring(1);
-                
-// obtaining input and out streams 
-                DataInputStream dis = new DataInputStream(s.getInputStream()); 
-                DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
-                  
-                System.out.println("Assigning new thread for this client"); 
-  
-                // create a new thread object 
-                Thread t = new ClientHandler(s, dis, dos,ip); 
-  
-                // Invoking the start() method 
-                t.start(); 
-                  
-            } 
-            catch (Exception e){ 
-                s.close(); 
-                e.printStackTrace(); 
-            } 
-        } 
-    } 
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        // server is listening on port 5057
+        ServerSocket ss = new ServerSocket(5057);
+
+        // running infinite loop for getting client requests
+        while (true) {
+            Socket s = null;
+            try {
+
+                // socket object to receive incoming client requests
+                s = ss.accept();
+
+                System.out.println("A new client is connected : " + s);
+
+                System.out.println("Ip Address  =  " + s.getInetAddress());
+
+                String temp = s.getInetAddress().toString();
+                String ip = temp.substring(1);
+
+                // obtaining input and out streams
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+                System.out.println("Assigning new thread for this client");
+
+                // create a new thread object
+                Thread t = new ClientHandler(s, dis, dos, ip);
+
+                // Invoking the start() method
+                t.start();
+
+            } catch (Exception e) {
+                s.close();
+                e.printStackTrace();
+            }
+        }
+    }
 }
 
-class ClientHandler extends Thread  
-{ 
-    DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd"); 
-    DateFormat fortime = new SimpleDateFormat("hh:mm:ss"); 
-    final DataInputStream dis; 
-    final DataOutputStream dos; 
-    final Socket s; 
+class ClientHandler extends Thread {
+
+    DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd");
+    DateFormat fortime = new SimpleDateFormat("hh:mm:ss");
+    final DataInputStream dis;
+    final DataOutputStream dos;
+    final Socket s;
     String ip_address;
-    int flag=0;
-  
-    // Constructor 
-    public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos,String ip_address)  
-    { 
-        this.s = s; 
-        this.dis = dis; 
-        this.dos = dos; 
-        this.ip_address=ip_address;
-    } 
-  
+    int flag = 0;
+    final String JDBC_Driver_Class = "com.mysql.jdbc.Driver";
+    final String DB_URL = "jdbc:mysql://localhost/peers?autoReconnect=true&useSSL=false";
+    final String USER = "root";
+    final String PASS = "pass123";
+
+    // Constructor
+    public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos, String ip_address) {
+        this.s = s;
+        this.dis = dis;
+        this.dos = dos;
+        this.ip_address = ip_address;
+    }
+
     @Override
-    public void run()  
-    { 
-        String received; 
-        String toreturn; 
-        while (true)  
-        { 
-            try { 
-                
-                if(flag==0)
-                // Ask user what he wants 
-                {
-                    dos.writeUTF("1"); 
+    public void run() {
+        String received;
+        String toreturn;
+        while (true) {
+            try {
+                if (flag == 0) {
+                    dos.writeUTF("1");
                     received = dis.readUTF();
-                    System.out.println("Mac of client : "+received);
-                    flag=1;
+                    System.out.println("Mac of client : " + received);
+                    flag = 1;
+                    int sig = 0;
+                    try {
+                        Class.forName(JDBC_Driver_Class);
+                        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                        Statement stmt = conn.createStatement();
+                        String sql = "SELECT * from peers where macaddress = '" + received + "';";
+
+                        ResultSet rs = stmt.executeQuery(sql);
+                        String ip_stored="";
+                        while (rs.next()) {
+                                ip_stored = rs.getString("ipaddress");
+                            }
+                        if (!ip_stored.equals("")) {
+                            System.out.println("Client Already registered ");
+                            
+                            if (ip_stored.equals(ip_address))
+                            {
+                                System.out.println("Same IPAddress");
+                                sig = 1;
+                            }
+                            else{
+                                System.out.println("Different IP address Need to update");
+                                sig = 2;
+                            }
+                        }
+                        stmt.close();
+                        if (sig == 2) {
+                            Statement stmt2 = conn.createStatement();
+                            String sql2 = "UPDATE peers SET ipaddress = '" + ip_address + "' where macaddress = '"
+                                    + received + "';";
+                            stmt2.executeUpdate(sql2);
+                            stmt2.close();
+                        }
+
+                        conn.close();
+
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    if (sig != 0)
+                        continue;
+
+                    try {
+                        Class.forName(JDBC_Driver_Class);
+                        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                        Statement stmt = conn.createStatement();
+                        String sql = "INSERT INTO peers VALUES ('" + received + "','" + ip_address + "');";
+                        stmt.executeUpdate(sql);
+                        stmt.close();
+                        conn.close();
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                } else {
+                    // receive the answer from client
+                    dos.writeUTF("Enter file to search");
+                    received = dis.readUTF();
+
+                    if (received.equals("Exit")) {
+                        System.out.println("Client " + this.s + " sends exit...");
+                        System.out.println("Closing this connection.");
+                        this.s.close();
+                        System.out.println("Connection closed");
+                        break;
+                    }
+
+                    // creating Date object
+                    Date date = new Date();
+
+                    // write on output stream based on the
+                    // answer from the client
+                    switch (received) {
+
+                    case "Date":
+                        toreturn = fordate.format(date);
+                        dos.writeUTF(toreturn);
+                        break;
+
+                    case "Time":
+                        toreturn = fortime.format(date);
+                        dos.writeUTF(toreturn);
+                        break;
+
+                    default:
+                        dos.writeUTF("Invalid input");
+                        break;
+                    }
                 }
-                else
-                {
-                        // receive the answer from client 
-                        dos.writeUTF("Enter file to search");
-                        received = dis.readUTF(); 
-                        
-                        if(received.equals("Exit")) 
-                        {  
-                            System.out.println("Client " + this.s + " sends exit..."); 
-                            System.out.println("Closing this connection."); 
-                            this.s.close(); 
-                            System.out.println("Connection closed"); 
-                            break; 
-                        } 
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-                        // creating Date object 
-                        Date date = new Date(); 
+        }
 
-                        // write on output stream based on the 
-                        // answer from the client 
-                        switch (received) { 
+        try {
+            // closing resources
+            this.dis.close();
+            this.dos.close();
 
-                            case "Date" : 
-                                toreturn = fordate.format(date); 
-                                dos.writeUTF(toreturn); 
-                                break; 
-
-                            case "Time" : 
-                                toreturn = fortime.format(date); 
-                                dos.writeUTF(toreturn); 
-                                break; 
-
-                            default: 
-                                dos.writeUTF("Invalid input"); 
-                                break; 
-                        } 
-                }
-            } catch (IOException e) { 
-                e.printStackTrace(); 
-            } 
-           
-        } 
-          
-        try
-        { 
-            // closing resources 
-            this.dis.close(); 
-            this.dos.close(); 
-              
-        }catch(IOException e){ 
-            e.printStackTrace(); 
-        } 
-    } 
-} 
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
